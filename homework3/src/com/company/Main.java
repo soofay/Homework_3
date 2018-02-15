ca213f56cc17fcb8332d939d443507a0a7d43d4b
SoonyonFay <35222283+soofay@users.noreply.github.com>
2018-02-15 17:18:45 +0000

    Update Main.java

--- homework3/src/com/company/Main.java
+++ homework3/src/com/company/Main.java
@@ -7,16 +7,12 @@ import com.company.nominee.Nominee;
 public class Main {
 
     public static void main(String[] args) {
-        Award award1 = new Award(100, 0.75);
-        Nominee nominee1 = new Nominee("Soo1");
-        Nominator nominator1 = new Nominator("Chloe1");
-        nominator1.nominate(nominee1,award1);
-
-        Award award2 = new Award(50);
-        Nominee nominee2 = new Nominee("Soo2");
-        Nominator nominator2 = new Nominator("Chloe2");
-        nominator2.nominate(nominee2, award2);
-
-
+        Award award = new Award(100, 0.75);
+        Award award2 = new Award(200, 1);
+        Nominee nominee = new Nominee("Soo1");
+        Nominator nominator = new Nominator("Chloe1");
+        //nominator.nominate(nominee,award);
+        nominee.receiveAward(award);
+        nominee.receiveAward(award2);
           }
 }
