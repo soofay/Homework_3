56c48a364a2e6e09a849f96f9761743c7299b0c0
SoonyonFay <35222283+soofay@users.noreply.github.com>
2018-02-16 15:46:39 +0000

    Update Main.java

--- homework3/src/com/company/Main.java
+++ homework3/src/com/company/Main.java
@@ -7,12 +7,46 @@ import com.company.nominee.Nominee;
 public class Main {
 
     public static void main(String[] args) {
-        Award award = new Award(100, 0.75);
-        Award award2 = new Award(200, 1);
-        Nominee nominee = new Nominee("Soo1");
-        Nominator nominator = new Nominator("Chloe1");
-        //nominator.nominate(nominee,award);
-        nominee.receiveAward(award);
-        nominee.receiveAward(award2);
-          }
+
+        Nominator nominator1 = new Nominator("Chloe");
+        Nominee nominee1 = new Nominee("Soo");
+        Award award1 = new Award(100, 0.75);
+        nominator1.nominate(nominator1, nominee1, award1);
+        nominee1.receiveAward(award1);
+
+        Award award2 = new Award(70);
+        nominator1.nominate(nominator1, nominee1, award2);
+        nominee1.receiveAward(award2);
+
+        Nominee nominee2 = new Nominee("Yena");
+        Award award3 = new Award( 100, 1.5);
+        nominator1.nominate(nominator1, nominee2, award3);
+        nominee2.receiveAward(award3);
+
+        Award award4 = new Award(200);
+        nominator1.nominate(nominator1, nominee2, award4);
+        nominee2.receiveAward(award4);
+
+        Award award5 = new Award(50, 2);
+        Nominator nominator2 = new Nominator("Kira");
+        Nominee nominee3 = new Nominee( "Mila");
+        nominator2.nominate(nominator2, nominee3, award5);
+        nominee3.receiveAward(award5);
+
+        Award award6 = new Award(50, 0.5);
+        nominator2.nominate(nominator2, nominee3, award6);
+        nominee3.receiveAward(award6);
+
+        nominator1.nominate(nominator1, nominee3,award2);
+        nominee2.receiveAward(award2);
+
+        nominator1.nominate(nominator1, nominee3,award4);
+        nominee2.receiveAward(award4);
+
+        nominator1.nominate(nominator1, nominee3,award2);
+        nominee2.receiveAward(award2);
+
+
+
+     }
 }
