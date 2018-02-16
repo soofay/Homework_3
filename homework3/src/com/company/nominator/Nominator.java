e0fb6d135966e73eab47281345e159d1efd5673f
SoonyonFay <35222283+soofay@users.noreply.github.com>
2018-02-16 15:45:16 +0000

    Update Nominator.java

--- homework3/src/com/company/nominator/Nominator.java
+++ homework3/src/com/company/nominator/Nominator.java
@@ -8,16 +8,17 @@ public class Nominator {
     private String name;
 
     static {
-        System.out.println("Nominator is initialized");
+        //System.out.println("Nominator is initialized");
     }
 
     public Nominator(String name) {
         this.name = name;
     }
 
-    public void nominate(Nominee nominee, Award award, ) {
-        System.out.println("Recipient name is " + nominee.getName());
-        System.out.println("Award amount is " + award.getValue());
+    public void nominate(Nominator nominator, Nominee nominee, Award award ) {
+        System.out.println("Nominator's name is " + nominator.name);
+        System.out.println("Recipient's name is " + nominee.getName());
+        //System.out.println("The nominated Award amount is " + award.getValue());
 
     }
 }
